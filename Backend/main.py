fbf815898ad0d1a00e119725ce231a1ffee27f28
Likhith Kumar B <likhith15kumar@gmail.com>
2026-04-05 14:16:56 +0530

    connected filescanner to backend

--- Backend/main.py
+++ Backend/main.py
@@ -1,18 +1,22 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import url_analyzer
+from routers import file_scanner          
+
 
 app = FastAPI(title="CyberPulse API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # your React frontend
+    allow_origins=["http://localhost:5173"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(url_analyzer.router, prefix="/url", tags=["URL Analyzer"])
+app.include_router(file_scanner.router, prefix="/file", tags=["File Scanner"])   
+
 
 @app.get("/")
 def root():
